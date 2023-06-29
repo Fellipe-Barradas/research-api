c9e4b82682ffc9c7ea1946e452e71bf64705061e
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-06-29 12:31:36 -0300

    Feito o CRUD do User

--- server/src/main/java/com/pointtech/server/resources/UserResource.java
+++ server/src/main/java/com/pointtech/server/resources/UserResource.java
@@ -1,13 +1,22 @@
 package com.pointtech.server.resources;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pointtech.server.entities.User;
 import com.pointtech.server.services.UserServices;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -25,6 +34,30 @@ public class UserResource {
 
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userServices.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        user = userServices.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userServices.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        user = userServices.update(id, user);
+        return ResponseEntity.ok().body(user);
+    }
     
     
 }
--- server/src/main/java/com/pointtech/server/services/UserServices.java
+++ server/src/main/java/com/pointtech/server/services/UserServices.java
@@ -18,4 +18,27 @@ public class UserServices {
         return userRepository.findAll();
     }
 
+    public User findById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    public User insert(User user) {
+        return userRepository.save(user);
+    }
+
+    public void delete(Long id){
+        userRepository.deleteById(id);
+    }
+
+    public User update(Long id, User user){
+        User entity = userRepository.getReferenceById(id);
+        updateData(entity, user);
+        return userRepository.save(entity);
+    }
+
+    private void updateData(User entity, User user) {
+        entity.setName(user.getName());
+        entity.setEmail(user.getEmail());
+        entity.setPassword(user.getPassword());
+    }
 }
