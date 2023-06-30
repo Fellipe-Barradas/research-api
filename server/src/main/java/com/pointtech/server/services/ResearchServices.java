6afcb525200132c4fc367f993ff217827e3d9be5
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-06-29 23:34:57 -0300

    Exceptions for researchies

--- server/src/main/java/com/pointtech/server/services/ResearchServices.java
+++ server/src/main/java/com/pointtech/server/services/ResearchServices.java
@@ -1,12 +1,17 @@
 package com.pointtech.server.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.pointtech.server.entities.Research;
 import com.pointtech.server.repositories.ResearchRepository;
+import com.pointtech.server.services.exceptions.DatabaseException;
+import com.pointtech.server.services.exceptions.ElementAlreadyExists;
+import com.pointtech.server.services.exceptions.ElementNotFounded;
 
 @Service
 public class ResearchServices {
@@ -19,21 +24,39 @@ public class ResearchServices {
     }
 
     public Research findById(Long id) {
-        return researchRepository.findById(id).get();
+        try{
+            return researchRepository.findById(id).get();
+        }catch (NoSuchElementException e) {
+            throw new ElementNotFounded(id);
+        }
+        
     }
 
     public Research insert(Research research) {
+        if(findById(research.getId()) != null){
+            throw new ElementAlreadyExists("User already exists!");
+        }
         return researchRepository.save(research);
     }
 
     public void delete(Long id){
-        researchRepository.deleteById(id);
+       try {
+            researchRepository.deleteById(id);
+        } catch (NoSuchElementException e) {
+            throw new ElementNotFounded(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException("Integrity violation");
+        }
     }
 
     public Research update(Long id, Research research){
-        Research entity = researchRepository.getReferenceById(id);
-        updateData(entity, research);
-        return researchRepository.save(entity);
+         try {
+            Research entity = researchRepository.getReferenceById(id);
+            updateData(entity, research);
+            return researchRepository.save(entity);
+        } catch (NoSuchElementException e) {
+            throw new ElementNotFounded(id);
+        }
     }
 
     private void updateData(Research entity, Research research) {
