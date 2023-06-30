3358a8f1f58507905c7f2aaf486516e497eefa2a
Luis Fellipe <luisbezerrabarradas@gmail.com>
2023-06-29 23:38:16 -0300

    Exceptions for ratings

--- server/src/main/java/com/pointtech/server/services/RatingServices.java
+++ server/src/main/java/com/pointtech/server/services/RatingServices.java
@@ -1,12 +1,17 @@
 package com.pointtech.server.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.pointtech.server.entities.Rating;
 import com.pointtech.server.repositories.RatingRepository;
+import com.pointtech.server.services.exceptions.DatabaseException;
+import com.pointtech.server.services.exceptions.ElementAlreadyExists;
+import com.pointtech.server.services.exceptions.ElementNotFounded;
 
 @Service
 public class RatingServices {
@@ -19,21 +24,38 @@ public class RatingServices {
     }
 
     public Rating findById(Long id) {
-        return ratingRepository.findById(id).get();
+        try {
+            return ratingRepository.findById(id).get();
+        } catch (NoSuchElementException e) {
+            throw new ElementNotFounded(id);
+        }  
     }
 
-    public Rating insert(Rating Rating) {
-        return ratingRepository.save(Rating);
+    public Rating insert(Rating rating) {
+         if(findById(rating.getId()) != null){
+            throw new ElementAlreadyExists("User already exists!");
+        }
+        return ratingRepository.save(rating);
     }
 
     public void delete(Long id){
-        ratingRepository.deleteById(id);
+        try {
+            ratingRepository.deleteById(id);
+        } catch (NoSuchElementException e) {
+            throw new ElementNotFounded(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException("Integrity violation");
+        }
     }
 
     public Rating update(Long id, Rating rating){
-        Rating entity = ratingRepository.getReferenceById(id);
-        updateData(entity, rating);
-        return ratingRepository.save(entity);
+       try {
+            Rating entity = ratingRepository.getReferenceById(id);
+            updateData(entity, rating);
+            return ratingRepository.save(entity);
+        } catch (NoSuchElementException e) {
+            throw new ElementNotFounded(id);
+        }
     }
 
     private void updateData(Rating entity, Rating rating) {
